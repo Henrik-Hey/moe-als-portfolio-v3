c70e054332d7e0de524eed43cbc74e20040f010f
Henrik Hey <henrikheywork@gmail.com>
2025-01-19 19:05:36 -0500

    wip

--- frontend/src/app/projects/shiba-inu/content.tsx
+++ frontend/src/app/projects/shiba-inu/content.tsx
@@ -56,13 +56,13 @@ export default function Content() {
       />
       <Container maxWidth="md">
         <Box display="flex" flexDirection="column" gap={12} my={12}>
-          <ProjectMissionStatement
+          {/* <ProjectMissionStatement
             typeTitle="Project Type"
             typeText="Bootcamp case study"
             roleAndResponsibilitiesText="Sketching, User Research, Wireframing, Visual Design, Prototyping, User Testing, Interaction Design, Brand Creation."
             platformsText="Mobile"
             timelineText="Q1 2022 - Q2 2022"
-          />
+          /> */}
           <ProjectStatement content="Crafting a distinctive product with a delightful user experience, Shiba Inu endeavors to compete with established market leaders. Centered on the needs of students and busy young adults, the app caters to those who crave exceptional Asian ethnic food but find themselves pressed for time to cook." />
           <FadeIn>
             <Box>
